2a7688c1c672341b3ed9dc4796b231395eaa26eb
Seth <sethsolidworksacct@gmail.com>
2024-07-21 11:41:49 -0400

    Renamed chapters to keep them in the correct order; finished Book 1

---
+++ Book 1/Chapter12/listing011203.js
@@ -0,0 +1,9 @@
+//oregoninfo.js
+
+export const stateName = "Oregon";
+export const capitalCity = "Salem";
+export const stateBird = "Western meadowlark";
+
+export default function getStateInfo() {
+  return { stateName, capitalCity, stateBird };
+}
